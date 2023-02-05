8193bf820bafddf20f21325a43e477d000c4d25e
Andrey Evseev <a-evseev@yandex.ru>
2023-02-05 17:09:12 +0300

    решение задания 4

---
+++ Task_4.py
@@ -0,0 +1,11 @@
+def descimal_to_binary(k: int) -> str:
+    bin_k = f'{k % 2}'
+    while k // 2 != 0:
+        k = k // 2
+        bin_k = f'{k % 2}' + bin_k
+    bin_k = int(bin_k)
+    return bin_k
+
+k = int(input('Введите натуральное число: '))
+b = descimal_to_binary(k)
+print(f'{k} -> {b}')
